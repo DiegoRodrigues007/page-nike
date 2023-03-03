3086e43abe85e4d21120659a72493e41063dbea9
Diego Rodrigues <diego.rodrigues3007@gmail.com>
2023-03-03 08:33:09 -0300

    alteração

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,22 +1,21 @@
+import "../CampoTexto/CampoTexto.css";
 
-import '../CampoTexto/CampoTexto.css';
-
-import Tenis from '../../img/tenis-branco.svg';
+import Tenis from "../../img/tenis-branco.svg";
 
 const CampoTexto = () => {
   return (
-    <section className='page-home'>
-      <div className='text'>
+    <section className="page-home">
+      <div className="text">
         <h1>Brand New Edition</h1>
         <p>
           WE VE SET BOLD AND AMBITIOUS TARGETS FOR 2025 AGAINST OUR FOCUS AREAS
           OS PEOPLE, PLANET AND PLAY. LEARN MORE ABOUT OUR JOURNEY TO A BETTER
           FUTURE
         </p>
-        <a className='button'>Shop</a>
+        <a className="button">Shop</a>
       </div>
       <div>
-        <img className = 'img' src={Tenis}/>
+        <img className="img" src={Tenis} />
       </div>
     </section>
   );
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,32 +1,33 @@
+import "../Menu/Menu.css";
 
-import '../Menu/Menu.css';
+import logo from "../../img/logo.svg";
+import compras from "../../img/compras.svg";
 
-import logo from '../../img/logo.svg';
-import compras from '../../img/compras.svg';
- 
+const rotas = [
+  { title: "Men", href: "./men" },
+  { title: "Womem", href: "./women" },
+  { title: "Kids", href: "./kids" },
+  { title: "Sale", href: "./sale" },
+];
 
-const Menu = ()=>{
+const Menu = () => {
+  return (
+    <header>
+      <nav className="menu">
+        <img className="logo" src={logo} alt="Logo" />
 
-    return(
+        <ul>
+          {rotas.map((rota) => (
+            <li key={rota.title}>
+              <a href={rota.href}>{rota.title}</a>
+            </li>
+          ))}
+        </ul>
 
-        <header >
-            <nav className='menu'>
-                <img className='logo' src={logo} alt='Logo'/> 
-
-                <ul>
-                    <li>Men</li>
-                    <li>Womem</li>
-                    <li>Kids</li>
-                    <li>Sale</li>
-                </ul>
-                
-                <img className='img-compras' src={compras} alt='Compras'/>
-            </nav>
-        </header>
-
-    )
-
-}
-
+        <img className="img-compras" src={compras} alt="Compras" />
+      </nav>
+    </header>
+  );
+};
 
 export default Menu;
